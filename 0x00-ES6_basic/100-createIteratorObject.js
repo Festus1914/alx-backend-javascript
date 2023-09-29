d5e0b8871296594bc11d36d2fbbf278da98c47ce
Festus1914 <oladiplentyfestus@gmail.com>
2023-09-29 19:15:43 +0000

    Initial commit

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,23 +1,25 @@
 export default function createIteratorObject(report) {
-  let departments = Object.values(report.allEmployees);
+  let departmentNames = Object.keys(report.allEmployees);
   let currentDepartmentIndex = 0;
   let currentEmployeeIndex = 0;
 
   return {
     next() {
-      if (currentDepartmentIndex >= departments.length) {
+      if (currentDepartmentIndex >= departmentNames.length) {
         return { done: true };
       }
 
-      const currentDepartment = departments[currentDepartmentIndex];
-      const currentEmployee = currentDepartment[currentEmployeeIndex];
+      const departmentName = departmentNames[currentDepartmentIndex];
+      const employeesInDepartment = report.allEmployees[departmentName];
 
-      currentEmployeeIndex++;
-
-      if (currentEmployeeIndex >= currentDepartment.length) {
+      if (currentEmployeeIndex >= employeesInDepartment.length) {
         currentDepartmentIndex++;
         currentEmployeeIndex = 0;
+        return this.next(); // Skip empty departments
       }
+
+      const currentEmployee = employeesInDepartment[currentEmployeeIndex];
+      currentEmployeeIndex++;
 
       return { value: currentEmployee, done: false };
     },
