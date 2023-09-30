12c03bc566b282bf201c30edeb96fbcd66f4fd77
Festus1914 <oladiplentyfestus@gmail.com>
2023-09-30 13:24:12 +0000

    Initial commit

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,30 +1,9 @@
 export default function createIteratorObject(report) {
-  let departmentNames = Object.keys(report.allEmployees);
-  let currentDepartmentIndex = 0;
-  let currentEmployeeIndex = 0;
-
-  return {
-    next() {
-      if (currentDepartmentIndex >= departmentNames.length) {
-        return { done: true };
+  return (function* _() {
+    for (const department of Object.values(report.allEmployees)) {
+      for (const employee of department) {
+        yield employee;
       }
-
-      const departmentName = departmentNames[currentDepartmentIndex];
-      const employeesInDepartment = report.allEmployees[departmentName];
-
-      if (currentEmployeeIndex >= employeesInDepartment.length) {
-        currentDepartmentIndex++;
-        currentEmployeeIndex = 0;
-        return this.next(); // Skip empty departments
-      }
-
-      const currentEmployee = employeesInDepartment[currentEmployeeIndex];
-      currentEmployeeIndex++;
-
-      return { value: currentEmployee, done: false };
-    },
-    [Symbol.iterator]() {
-      return this;
-    },
-  };
+    }
+  }());
 }
