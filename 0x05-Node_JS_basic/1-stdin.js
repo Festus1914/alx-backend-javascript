9fdc22fdefc53cf5689f3fe1f0ba6df1be636767
Festus1914 <oladiplentyfestusu@gmail.com>
2023-11-21 09:05:51 +0000

    Using Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,23 +1,16 @@
-// 1-stdin.js
+console.log('Welcome to Holberton School, what is your name?');
 
-process.stdout.write("Welcome to Holberton School, what is your name?\n");
-
-process.stdin.setEncoding('utf8');
-
-process.stdin.on('data', function(data) {
-  const name = data.trim();
-  
-  if (name) {
-    process.stdout.write(`Your name is: ${name}\n`);
-  } else {
-    process.stdout.write("Your name is: \n");
-  }
-
-  process.stdout.write("This important software is now closing\n");
-  process.exit();
-});
-
-process.on('SIGINT', function() {
-  process.stdout.write("This important software is now closing\n");
-  process.exit();
-});
+if (process.stdin.isTTY) {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data}`);
+    process.exit();
+  });
+} else {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data}`);
+    process.exit();
+  });
+  process.on('exit', () => {
+    console.log('This important software is now closing');
+  });
+}
