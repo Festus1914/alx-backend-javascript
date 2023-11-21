903f25300d43f2d41b92eae55ae3180093a1bac3
Festus1914 <oladiplentyfestusu@gmail.com>
2023-11-21 08:57:28 +0000

    Using Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,23 +1,12 @@
-//Create a program named 1-stdin.js that will be executed through command line.
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdout.write("Welcome to Holberton School, what is your name?\n");
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
 
-process.stdin.setEncoding('utf8');
-
-process.stdin.on('data', function(data) {
-  const name = data.trim();
-  
-  if (name) {
-    process.stdout.write(`Your name is: ${name}\n`);
-  } else {
-    process.stdout.write("Your name is: \n");
+  if (chunk) {
+    process.stdout.write(`Your name is: ${chunk}`);
   }
-
-  process.stdout.write("This important software is now closing\n");
-  process.exit();
 });
 
-process.on('SIGINT', function() {
-  process.stdout.write("This important software is now closing\n");
-  process.exit();
-});
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
