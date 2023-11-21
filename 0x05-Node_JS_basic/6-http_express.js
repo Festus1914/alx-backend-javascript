8f53b741fd79ef8e02ef12c5fff9db29e2c8195d
Festus1914 <oladiplentyfestusu@gmail.com>
2023-11-21 09:46:07 +0000

    Create a small HTTP server using Express

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,13 +1,33 @@
 const express = require('express');
 const app = express();
-const PORT = 1245;
 
-app.get('/', (_, res) => {
+const port = 1245;
+
+// Define the route for the endpoint '/'
+app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server listening on PORT ${PORT}`);
+// Handle 404 errors for undefined routes
+app.use((req, res) => {
+  res.status(404).send(`
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="utf-8">
+      <title>Error</title>
+    </head>
+    <body>
+      <pre>Cannot GET ${req.url}</pre>
+    </body>
+    </html>
+  `);
 });
 
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
+
+// Export the app variable
 module.exports = app;
