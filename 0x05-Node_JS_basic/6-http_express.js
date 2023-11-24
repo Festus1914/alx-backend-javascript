a18a8a21e14f640baf6b164d200a9659b824c630
Festus1914 <oladiplentyfestusu@gmail.com>
2023-11-24 13:53:52 +0000

    Adding files

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,33 +1,13 @@
 const express = require('express');
-const app = express();
 
+const app = express();
 const port = 1245;
 
-// Define the route for the endpoint '/'
-app.get('/', (req, res) => {
-  res.send('Hello Holberton School!');
+app.get('/', (request, response) => {
+  response.send('Hello Holberton School!');
 });
 
-// Handle 404 errors for undefined routes
-app.use((req, res) => {
-  res.status(404).send(`
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-      <meta charset="utf-8">
-      <title>Error</title>
-    </head>
-    <body>
-      <pre>Cannot GET ${req.url}</pre>
-    </body>
-    </html>
-  `);
-});
-
-// Start the server
 app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
 });
 
-// Export the app variable
 module.exports = app;
